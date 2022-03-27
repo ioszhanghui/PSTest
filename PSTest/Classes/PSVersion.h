88655db5fee2c7ad7e860f474fb96132aabc8a64
zhanghui119 <zhanghui119@gome.com.cn>
2022-03-27 09:31:53 +0800

    1.0.0

---
+++ PSTest/Classes/PSVersion.h
@@ -0,0 +1,16 @@
+//
+//  PSText.h
+//  PSTest
+//
+//  Created by apple on 2022/3/27.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PSVersion : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PSTest/Classes/PSVersion.m
@@ -0,0 +1,12 @@
+//
+//  PSText.m
+//  PSTest
+//
+//  Created by apple on 2022/3/27.
+//
+
+#import "PSVersion.h"
+
+@implementation PSVersion
+
+@end
